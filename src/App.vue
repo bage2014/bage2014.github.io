3c7589b122b9f5165d64b9fc05da32dbca852ecf
bage2014 <893542907@qq.com>
2020-04-11 14:25:54 +0800

    menu

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,45 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <!-- App.vue -->
+
+    <v-app>
+      <v-navigation-drawer app>
+        <!-- -->
+        <nav-menu></nav-menu>
+      </v-navigation-drawer>
+
+      <v-app-bar app>
+        <!-- -->
+      </v-app-bar>
+
+      <!-- Sizes your content based upon application components -->
+      <v-content>
+
+        <!-- Provides the application the proper gutter -->
+        <v-container fluid>
+
+          <!-- If using vue-router -->
+          <router-view></router-view>
+        </v-container>
+      </v-content>
+
+      <v-footer app>
+        <!-- -->
+      </v-footer>
+    </v-app>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
+
+import NavMenu from '@/components/NavMenu'
+
+
+export default {  
+  name: 'App',
+  components: {
+    'nav-menu': NavMenu,
+  },
 }
 </script>
 
