327047546332d06f148e3df5f68c65b519f2f33c
bage2014 <893542907@qq.com>
2020-04-11 12:09:39 +0800

    reset

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,23 @@
 <template>
-  <v-app light>
-    <v-navigation-drawer 
-      v-if="authenticated" 
-      persistent 
-      v-model="drawer" 
-      enable-resize-watcher 
-      app
-    >
-      <nav-menu></nav-menu>
-    </v-navigation-drawer>
-    <tool-bar v-on:toggleDrawer="drawer = !drawer" :drawer="drawer"></tool-bar>
-    <v-content>
-      <v-container fluid>
-        <transition name="page" mode="out-in">
-          <router-view></router-view>
-        </transition>
-      </v-container>
-    </v-content>
-    <feedback-message></feedback-message>
-    <page-footer></page-footer>
-  </v-app>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
 </template>
 
-
 <script>
-import { mapGetters } from 'vuex'
-
-import NavMenu from './components/NavMenu'
-import ToolBar from './components/ToolBar'
-import FeedbackMessage from './components/FeedbackMessage'
-import PageFooter from './components/PageFooter'
-
 export default {
-  components: {
-    'nav-menu': NavMenu,
-    'tool-bar': ToolBar,
-    'feedback-message': FeedbackMessage,
-    'page-footer': PageFooter
-  },
-
-  computed: mapGetters({
-    authenticated: 'authCheck'
-  }),
-
-  data () {
-    return {
-      drawer: true
-    }
-  }
+  name: 'App'
 }
 </script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
