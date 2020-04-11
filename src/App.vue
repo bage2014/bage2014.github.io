caca756bc01e6e30abf623927c46655c496e8f67
bage2014 <893542907@qq.com>
2020-04-11 10:42:54 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
@@ -10,18 +11,13 @@ export default {
 }
 </script>
 
-<style lang="less">
-.size{
-  width: 100%;
-  height: 100%;
-}
-html,body{
-  .size;
-  overflow: hidden;
-  margin: 0;
-  padding: 0;
-}
+<style>
 #app {
-  .size;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
