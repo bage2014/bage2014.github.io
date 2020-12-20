ec7b059d49feee3ec72bc683b5c616300f778999
bage2014 <893542907@qq.com>
2020-12-20 19:19:02 +0800

    first version

---
+++ precache-manifest.fbc325af97b124cc4c425d52e8a36532.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "aefefb0baa4553f9297b",
+    "url": "js/chunk-vendors.e6169e60.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "ff7527f8db3fb1482511",
+    "url": "js/example.51c4c2c7.js"
+  },
+  {
+    "revision": "3f11c241d539f371fed0",
+    "url": "js/chunk-2d0e9d44.f23ba206.js"
+  },
+  {
+    "revision": "2a88f408014f50b52fd9",
+    "url": "js/app.ec7af018.js"
+  },
+  {
+    "revision": "9b61f80580c1e30ed9e5",
+    "url": "js/about.846d435c.js"
+  },
+  {
+    "revision": "0c8b06c723c030ae86b45bb789372a53",
+    "url": "index.html"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "2a88f408014f50b52fd9",
+    "url": "css/app.20cab7f8.css"
+  },
+  {
+    "revision": "9b61f80580c1e30ed9e5",
+    "url": "css/about.7e2a0bf7.css"
+  }
+];
